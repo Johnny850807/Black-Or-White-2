58c8c20168a249a2922b70850db2bec40878854f
nyngwang <nyngwang@gmail.com>
2019-04-03 22:55:02 +0800

    Sprite builder

--- bow/bow-engine/src/main/java/com/pokewords/framework/sprites/SpriteBuilder.java
+++ bow/bow-engine/src/main/java/com/pokewords/framework/sprites/SpriteBuilder.java
@@ -2,4 +2,36 @@ package com.pokewords.framework.sprites;
 
 public class SpriteBuilder {
 
+
+    /**
+     * Requirements:
+     *  1. general
+     *  2.
+     *
+     * How should the API looks like:
+     *
+     *  SpriteBuilder.createNewSprite().setFrames().setB().setC().build();
+     *
+     *
+     * @return
+     */
+
+
+    private static Sprite sprite;
+
+    public static Class<SpriteBuilder> createNewSprite() {
+
+        sprite = new Sprite();
+
+        return SpriteBuilder.class;
+    }
+
+    public static Class<SpriteBuilder> methodB() {
+        return SpriteBuilder.class;
+    }
+
+    public static Class<SpriteBuilder> methodC() {
+        return SpriteBuilder.class;
+    }
+
 }
