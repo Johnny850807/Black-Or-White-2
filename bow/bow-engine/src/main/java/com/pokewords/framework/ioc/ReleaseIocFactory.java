3ce76da4e1dfd58f9316c73b14695e4d080c040b
johnny850807 <johnny850807@gmail.com>
2019-04-06 13:16:52 +0800

    init

--- bow/bow-engine/src/main/java/com/pokewords/framework/ioc/ReleaseIocFactory.java
+++ bow/bow-engine/src/main/java/com/pokewords/framework/ioc/ReleaseIocFactory.java
@@ -1,4 +1,28 @@
 package com.pokewords.framework.ioc;
 
-public class ReleaseIocFactory {
+import com.pokewords.framework.engine.UserConfig;
+import com.pokewords.framework.engine.asm.loader.scriptparser.PrototypeParser;
+import com.pokewords.framework.sprites.PrototypeFactory;
+import com.pokewords.framework.views.SoundPlayer;
+
+public class ReleaseIocFactory implements IocFactory{
+    @Override
+    public PrototypeParser prototypeParser() {
+        return null;
+    }
+
+    @Override
+    public PrototypeFactory prototypeFactory() {
+        return null;
+    }
+
+    @Override
+    public SoundPlayer soundPlayer() {
+        return null;
+    }
+
+    @Override
+    public UserConfig userConfig() {
+        return null;
+    }
 }
--- bow/bow-engine/src/test/java/com/pokewords/framework/sprites/PrototypeFactoryTest.java
+++ bow/bow-engine/src/test/java/com/pokewords/framework/sprites/PrototypeFactoryTest.java
@@ -1,5 +1,40 @@
+package com.pokewords.framework.sprites;
+
+import com.pokewords.framework.ioc.ReleaseIocFactory;
+import com.pokewords.framework.sprites.components.FrameStateMachineComponent;
+import com.pokewords.framework.sprites.components.PropertiesComponent;
+import com.pokewords.framework.sprites.components.TextureFrame;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PrototypeFactoryTest {
+    private PrototypeFactory prototypeFactory = new ReleaseIocFactory().prototypeFactory();
+    private Sprite sprite;
 
+    @Before
+    public void setup(){
+        sprite = new Sprite();
+        PropertiesComponent propertiesComponent = new PropertiesComponent();
+        FrameStateMachineComponent frameStateMachineComponent = new FrameStateMachineComponent();
+        frameStateMachineComponent.addState(new TextureFrame());
+        sprite.setPropertiesComponent(propertiesComponent);
+    }
+
+
+    @Test
+    public void testCloneExistentPrototype(){
+
+    }
+
+    @Test
+    public void testCloneNonexistentPrototypeShouldThrowException(){
+
+    }
+
+    @Test
+    public void testAddAndRemovePrototype(){
+
+    }
 }
