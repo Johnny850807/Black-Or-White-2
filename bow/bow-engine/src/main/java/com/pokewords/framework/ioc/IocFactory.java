097e6aca5d159ce0d13f338dc6620cc88e49dd6d
johnny850807 <johnny850807@gmail.com>
2019-04-06 23:23:18 +0800

    encapsulate releasefactory

--- bow/bow-engine/src/main/java/com/pokewords/framework/ioc/IocFactory.java
+++ bow/bow-engine/src/main/java/com/pokewords/framework/ioc/IocFactory.java
@@ -1,11 +1,14 @@
 package com.pokewords.framework.ioc;
 
+import com.pokewords.framework.engine.FrameFactory;
 import com.pokewords.framework.engine.asm.loader.scriptparser.PrototypeParser;
 import com.pokewords.framework.sprites.PrototypeFactory;
 import com.pokewords.framework.views.SoundPlayer;
 import com.pokewords.framework.engine.UserConfig;
 
 public interface IocFactory {
+
+	FrameFactory frameFactory();
 
 	PrototypeParser prototypeParser();
 
--- bow/bow-engine/src/main/java/com/pokewords/framework/ioc/ReleaseIocFactory.java
+++ bow/bow-engine/src/main/java/com/pokewords/framework/ioc/ReleaseIocFactory.java
@@ -1,11 +1,19 @@
 package com.pokewords.framework.ioc;
 
+import com.pokewords.framework.engine.FrameFactory;
+import com.pokewords.framework.engine.TextureFrameFactory;
 import com.pokewords.framework.engine.UserConfig;
 import com.pokewords.framework.engine.asm.loader.scriptparser.PrototypeParser;
 import com.pokewords.framework.sprites.PrototypeFactory;
 import com.pokewords.framework.views.SoundPlayer;
 
 public class ReleaseIocFactory implements IocFactory{
+
+    @Override
+    public FrameFactory frameFactory() {
+        return new TextureFrameFactory();
+    }
+
     @Override
     public PrototypeParser prototypeParser() {
         return null;
