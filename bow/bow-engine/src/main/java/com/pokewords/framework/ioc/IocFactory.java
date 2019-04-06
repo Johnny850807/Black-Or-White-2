906bdc986719f1eb9d16d7d2304cf88d5e06ab37
johnny850807 <johnny850807@gmail.com>
2019-04-06 23:25:07 +0800

    class diagram add framefactory

--- bow/bow-engine/src/main/java/com/pokewords/framework/ioc/IocFactory.java
+++ bow/bow-engine/src/main/java/com/pokewords/framework/ioc/IocFactory.java
@@ -1,6 +1,6 @@
 package com.pokewords.framework.ioc;
 
-import com.pokewords.framework.engine.FrameFactory;
+import com.pokewords.framework.sprites.components.FrameFactory;
 import com.pokewords.framework.engine.asm.loader.scriptparser.PrototypeParser;
 import com.pokewords.framework.sprites.PrototypeFactory;
 import com.pokewords.framework.views.SoundPlayer;
--- bow/bow-engine/src/main/java/com/pokewords/framework/ioc/ReleaseIocFactory.java
+++ bow/bow-engine/src/main/java/com/pokewords/framework/ioc/ReleaseIocFactory.java
@@ -1,6 +1,6 @@
 package com.pokewords.framework.ioc;
 
-import com.pokewords.framework.engine.FrameFactory;
+import com.pokewords.framework.sprites.components.FrameFactory;
 import com.pokewords.framework.engine.TextureFrameFactory;
 import com.pokewords.framework.engine.UserConfig;
 import com.pokewords.framework.engine.asm.loader.scriptparser.PrototypeParser;
