ee45504dcf78011a45498779156ae3e939292e22
johnny850807 <johnny850807@gmail.com>
2019-04-14 02:32:07 +0800

    revise the demo

--- bow/bow-engine/src/main/java/com/pokewords/framework/sprites/parsing/LinScript.java
+++ bow/bow-engine/src/main/java/com/pokewords/framework/sprites/parsing/LinScript.java
@@ -1,7 +1,7 @@
 package com.pokewords.framework.sprites.parsing;
 
 /**
- * This is called LinScript so that we always remember the time Lin contributes to the Script Definition.
+ * This is called LinScript so that we always remember the time Lin contributed to the Script Definition.
  */
 public class LinScript implements Script {
 
