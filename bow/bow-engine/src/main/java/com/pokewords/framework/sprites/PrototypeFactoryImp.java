4bc8cee41b84b1374cbb11d30410c8ddbf6512a7
johnny850807 <johnny850807@gmail.com>
2019-04-15 00:31:03 +0800

    script revision (gallery segment)

---
+++ bow/bow-engine/src/main/java/com/pokewords/framework/sprites/PrototypeFactoryImp.java
@@ -0,0 +1,22 @@
+package com.pokewords.framework.sprites;
+
+import java.util.Map;
+
+public class PrototypeFactoryImp implements PrototypeFactory{
+    private Map<>
+    @Override
+    public Sprite cloneSprite(String type) {
+        return null;
+    }
+
+    @Override
+    public void addPrototype(String type, Sprite prototype) {
+
+    }
+
+    @Override
+    public void removePrototype(String type) {
+
+    }
+
+}
--- bow/bow-engine/src/test/java/com/pokewords/framework/sprites/parsing/FrameStateMachineScriptParserTest.java
+++ bow/bow-engine/src/test/java/com/pokewords/framework/sprites/parsing/FrameStateMachineScriptParserTest.java
@@ -15,51 +15,56 @@ public class FrameStateMachineScriptParserTest extends AbstractTest {
     FrameFactory frameFactory = mock.frameFactory();
 
     final String SCRIPT =
-            "<profile>\n" +
-            "    gallery(0-59): sprite\\sys\\henry_f.bmp w: 50  h: 50  row: 10  col: 10\n" +
-            "    gallery(60-100): sprite\\sys\\henry_s.bmp w: 50  h: 50  row: 10  col: 10\n" +
-            "</profile>\n" +
-            "\n" +
-            "<frame> 0 punch\n" +
-            "   pic: 0  duration: 1  next: 1  dvx: 0  dvy: 0 \n" +
-            "   <bow>\n" +
-            "       state: 0\n" +
-            "   </bow>\n" +
-            "   <body>\n" +
-            "      x: 28  y: 34  w: 42  h: 19\n" +
-            "   </body>\n" +
-            "</frame>\n" +
-            "\n" +
-            "<frame> 1 punch\n" +
-            "   pic: 1  duration: 1  next: 2  dvx: 0  dvy: 0 \n" +
-            "   <bow>\n" +
-            "       state: 0\n" +
-            "   </bow>\n" +
-            "   <body>\n" +
-            "      x: 28  y: 34  w: 42  h: 19\n" +
-            "   </body>\n" +
-            "</frame>\n" +
-            "\n" +
-            "<frame> 2 punch\n" +
-            "   pic: 2  duration: 1  next: 3  dvx: 0  dvy: 0 \n" +
-            "   <bow>\n" +
-            "       state: 0\n" +
-            "   </bow>\n" +
-            "   <body>\n" +
-            "      x: 28  y: 34  w: 42  h: 19\n" +
-            "   </body>\n" +
-            "</frame>\n" +
-            "\n" +
-            "<frame> 3 punch\n" +
-            "   pic: 3  duration: 1  next: 0  dvx: -1  dvy: 0 \n" +
-            "   sound: sound\\s24.wav\n" +
-            "   <bow>\n" +
-            "       state: 0 injury: 10 \n" +
-            "   </bow>\n" +
-            "   <body>\n" +
-            "      x: 28  y: 34  w: 42  h: 19\n" +
-            "   </body>\n" +
-            "</frame>";
+            "<gallery>\n" +
+             "    startPic: 0 endPic: 59 path: sprite\\sys\\henry_f.bmp\n" +
+             "    w: 50  h: 50  row: 10  col: 10\n" +
+             "</gallery>\n" +
+             "\n" +
+             "<gallery>\n" +
+             "    startPic: 60 endPic: 100 path: sprite\\sys\\henry_f.bmp\n" +
+             "    w: 50  h: 50  row: 10  col: 10\n" +
+             "</gallery>\n" +
+             "\n" +
+             "<frame> 0 punch\n" +
+             "   pic: 0  duration: 1  next: 1  dvx: 0  dvy: 0 \n" +
+             "   <bow>\n" +
+             "       state: 0\n" +
+             "   </bow>\n" +
+             "   <body>\n" +
+             "      x: 28  y: 34  w: 42  h: 19\n" +
+             "   </body>\n" +
+             "</frame>\n" +
+             "\n" +
+             "<frame> 1 punch\n" +
+             "   pic: 1  duration: 1  next: 2  dvx: 0  dvy: 0 \n" +
+             "   <bow>\n" +
+             "       state: 0\n" +
+             "   </bow>\n" +
+             "   <body>\n" +
+             "      x: 28  y: 34  w: 42  h: 19\n" +
+             "   </body>\n" +
+             "</frame>\n" +
+             "\n" +
+             "<frame> 2 punch\n" +
+             "   pic: 2  duration: 1  next: 3  dvx: 0  dvy: 0 \n" +
+             "   <bow>\n" +
+             "       state: 0\n" +
+             "   </bow>\n" +
+             "   <body>\n" +
+             "      x: 28  y: 34  w: 42  h: 19\n" +
+             "   </body>\n" +
+             "</frame>\n" +
+             "\n" +
+             "<frame> 3 punch\n" +
+             "   pic: 3  duration: 1  next: 0  dvx: -1  dvy: 0 \n" +
+             "   sound: sound\\s24.wav\n" +
+             "   <bow>\n" +
+             "       state: 0 injury: 10 \n" +
+             "   </bow>\n" +
+             "   <body>\n" +
+             "      x: 28  y: 34  w: 42  h: 19\n" +
+             "   </body>\n" +
+             "</frame>";
 
     /**
      * Because the parser triggers the listener from the first frame to the last frame,
