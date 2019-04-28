8470790cf248dffa3fec6cc7f1f3e858e98591b1
shawnwu4mac <shawnishere0806>
2019-04-28 18:59:13 +0800

    complete FiniteStateMachine

--- bow/bow-engine/src/main/java/com/pokewords/framework/engine/FiniteStateMachine.java
+++ bow/bow-engine/src/main/java/com/pokewords/framework/engine/FiniteStateMachine.java
@@ -8,115 +8,231 @@ import java.util.*;
  * @author Shawn
  */
 public class FiniteStateMachine<T> implements Cloneable{
-
 	/**
-	 * Increase according to the amount of Nodes
+	 * Point to the current state
 	 */
-	private int nodeNumber = 0;
-
-	private ArrayList<T> stateList = new ArrayList<>();
-	/**
-	 * ArrayList save StateNode
-	 */
-	private ArrayList<ArrayList<StateNode>> matrix = new ArrayList<>();
-
 	private T currentState;
 
 	/**
-	 * stateNodesMap is to save new nodes
+	 * Index of each event.
+	 * Give each event an index to operate in matrix
+	 */
+	private int eventIndex = 0;
+
+	/**
+	 * Use index to save event.
+	 */
+	private Map<String, Integer> eventMap = new HashMap<>();
+
+	/**
+	 * Save each state in the List, its index is the index of the state
+	 */
+	private ArrayList<T> stateList = new ArrayList<>();
+
+	/**
+	 * matrix for storing events and states
+	 *
+	 * If current state is A, its index is 0.
+	 * The index value of WakeUp event is 25,
+	 * and the state triggered by the WakeUp event is B.
+	 *
+	 * Then B will be stored like this:
+	 * matrix.get(0).get(25)
+	 */
+	private ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+
+	/**
+	 * Save each state,key is hashcode of state.
 	 */
 	private Map<Integer, StateNode> stateNodesMap = new HashMap<>();
 
+
 	/**
-	 * Show the overall status and currentState of the current 2D ArrayList.
+	 * Show the overall status and currentState of the current matrix.
 	 * The first number of each line is the index of the first state of each line,
+
 	 * e.g. 3|(0)122,(1)target,(2)z --> first state is '122' and it's index is 3.
 	 *
-	 * @return 2D ArrayList result
+	 * If the other states are the same (pointing to itself), use "-" instead
+	 *
+	 * e.g. originally might be like this:
+	 *  2|(0)2,(1)2,(2)2,(3)1,(4)2,(5)2
+	 *  become to ->
+	 *  2|(0)2,-,-,(3)1,-,-
+	 *
+	 * @return Complete matrix string
 	 */
 	@Override
 	public String toString() {
 		StringBuilder result = new StringBuilder();
-		result.append("Current State：").append(String.valueOf(currentState));
+		result.append("Current State：").append(currentState);
 		result.append("\n\n");
 		for(int i = 0; i < matrix.size(); i++){
 			result.append(i).append("|");
 			for(int j = 0; j < matrix.get(i).size(); j++){
-				result.append("(").append(j).append(")");
-				result.append(matrix.get(i).get(j).getState());
+				if(matrix.get(i).get(j).equals(matrix.get(i).get(0)) &&
+						j != 0){
+					result.append("-");
+				}else {
+					result.append("(").append(j).append(")");
+					result.append(matrix.get(i).get(j));
+				}
 				result.append(",");
 			}
 			result.deleteCharAt(result.length()-1);
 			result.append("\n");
 		}
-		System.out.println(result.toString());
 		return result.toString();
 	}
 
-
 	/**
-	 * The trigger function is able to switch different state
-	 * @param event Trigger of event
-	 * @return Return current state
+	 *  through the event to trigger the corresponding state, so that currentState points to the triggered state.
+	 *  If the event does not exist, do nothing and currentState is returned.
+	 *e.g.
+	 *   fsm.addState(A);
+	 *   fsm.addState(B);
+	 *   fsm.addState(C);
+	 *   fsm.addState(D);
+	 *   fsm.addState(E);
+	 *   fsm.addState(F);
+	 *   fsm.addState(G);
+	 *   fsm.addState(H);
+	 *
+	 *   fsm.addTransition(A,WakeUp,H);
+	 *   fsm.addTransition(B,GO,H);
+	 *   fsm.addTransition(D,Sleep,C);
+	 *   fsm.addTransition(B,WakeUp,E);
+	 *   fsm.addTransition(C,Sleep,A);
+	 *
+	 *   e.g.
+	 *    fsm.setCurrentState(A);
+	 *    fsm.trigger(WakeUp);
+	 *    At this point, currentState will point to H.
+	 *
+	 *   e.g.
+	 *    fsm.setCurrentState(D);
+	 * 	  fsm.trigger(Sleep);
+	 *    At this point, currentState will point to C.
+	 *
+	 *   e.g.
+	 *    fsm.setCurrentState(B);
+	 * 	  fsm.trigger(Sleep);
+	 *    At this point, currentState is still B, because the GO event does not trigger any state.
+	 * @param event trigger event name
+	 * @return currentState
 	 */
-	public T trigger(String event) {
+	public T trigger(String event){
 		if(currentState == null){
 			throw new FiniteStateMachineException("currentState not exists");
 		}
-		StateNode currentNode = stateNodesMap.get(currentState.hashCode());
-		if(currentNode.getTargetNumberByCurrentNode(event) == null) {
-			return currentState;
-		}
-		int startNumber = currentNode.getStateNumber();
-		int targetNumber = currentNode.getTargetNumberByCurrentNode(event);
-		for(int i = 0; i < matrix.get(startNumber).size(); i++){
-			StateNode stateNode = matrix.get(startNumber).get(i);
-			if(stateNode.getStateNumber() == targetNumber){
-				currentState = matrix.get(startNumber).get(i).getState();
-			}
+		if(isEventExists(event)){
+			int eventIndex = getEventIndexFromEventMap(event);
+			StateNode currentNode = getStateNodeByState(currentState);
+			int currentNumber = matrix.get(currentNode.getStateNumber()).get(eventIndex);
+			currentState = stateList.get(currentNumber);
 		}
 		return currentState;
 	}
 
 	/**
-	 *
-	 * @param currentState current state
-	 * @throws FiniteStateMachineException
+	 * Replaces currentState
+	 * @param currentState to be replaced
 	 */
-	public void setCurrentState(T currentState) throws FiniteStateMachineException {
-		if(stateNodesMap.get(currentState.hashCode()) == null){
-			throw new FiniteStateMachineException("Not exists");
+	public void setCurrentState(T currentState){
+		if(!isStateExists(currentState)){
+            throw new FiniteStateMachineException("State not exist");
 		}else {
-			StateNode stateNode = stateNodesMap.get(currentState.hashCode());
-			this.currentState = stateNode.getState();
+			this.currentState = getStateNodeByState(currentState).getState();
 		}
 	}
 
 	/**
-	 * Return current state
+	 * Obtain the stateNode by @param state
+	 * @param t the state
+	 * @return mapped Statenode
+	 */
+	private StateNode getStateNodeByState(T t){
+		return stateNodesMap.get(t.hashCode());
+	}
+
+	/**
+	 * @return <tt>true</tt> if this state exists.
+	 */
+	private boolean isStateExists(T t){
+		return stateNodesMap.containsKey(t.hashCode());
+	}
+
+	/**
+	 * @return <tt>true</tt> if this event exists.
+	 */
+	private boolean isEventExists(String event){
+		return eventMap.containsKey(event);
+	}
+
+	/**
+	 * @param event event
+	 * @return event index if this event exists.
+	 */
+	private int getEventIndexFromEventMap(String event){
+		return eventMap.get(event);
+	}
+
+	/**
+	 * Associates @param event with the event index in eventToIndexMap.
+	 * Add an event if the event not exists.
+	 */
+	private void addEvent(String event){
+		if(!isEventExists(event)) {
+			eventMap.put(event, eventIndex++);
+		}
+	}
+
+	/**
 	 * @return current state
 	 */
-	public T getCurrentState() {
+	public T getCurrentState(){
 		return currentState;
 	}
 
-	/**
-	 * Add a new state to stateNodesMap
-	 * @param t element to be added to this Map
-	 */
-	public void addState(T t) {
-		stateList.add(t);
-		int stateNumber = nodeNumber++;
-		StateNode newNode = new StateNode(t,stateNumber);
-		stateNodesMap.put(t.hashCode(), newNode);
-		ArrayList<StateNode> newArrayInList = new ArrayList<>();
-		newArrayInList.add(newNode);
-		matrix.add(newArrayInList);
-	}
 
 	/**
-	 * @return all states
+	 * Add a new state to the stateNodesMap using hashcode as the key ,
+	 * and automatically align table lengths.
+	 *
+	 * The initial value of each column is @param state
+	 *
+	 * e.g.if the matrix look like:
+	 *  |1,2,3
+	 * =======
+	 * 1|1,1,1
+	 * 2|2,2,2
+	 * 3|3,3,3
+	 *
+	 * When a new node added, it will look like this:
+	 *  |1,2,3,4
+	 * =========
+	 * 1|1,1,1,1
+	 * 2|2,2,2,2
+	 * 3|3,3,3,3
+	 * 4|4,4,4,4
+	 *
+	 * @param t new state
 	 */
+	public void addState(T t){
+		stateList.add(t);
+		int stateNumber = stateList.size()-1;
+		StateNode newNode = new StateNode(t,stateNumber);
+		stateNodesMap.put(t.hashCode(), newNode);
+		ArrayList<Integer> initEventList = new ArrayList<>();
+		initEventList.add(stateNumber);
+		matrix.add(initEventList);
+		for(int i = 0; i < matrix.size(); i++) {
+			while(matrix.get(i).size() != matrix.size()){
+				matrix.get(i).add(matrix.get(i).get(0));
+			}
+		}
+	}
+
 	public ArrayList<T> getStates(){
 		return stateList;
 	}
@@ -127,49 +243,63 @@ public class FiniteStateMachine<T> implements Cloneable{
 	 * @param event the triggering event's name
 	 * @param to is triggered state
 	 */
-	public void addTransition(T from, String event, T to) {
-		if(stateNodesMap.get(from.hashCode()) == null || stateNodesMap.get(to.hashCode()) == null){
+	public void addTransition(T from, String event, T to){
+		if(!isStateExists(from) || !isStateExists(to)){
 			throw new FiniteStateMachineException("Not exists");
 		}else {
-			StateNode fromNode = stateNodesMap.get(from.hashCode());
-			StateNode toNode = stateNodesMap.get(to.hashCode());
-			int fromNumber = fromNode.getStateNumber();
-			int toNumber = toNode.getStateNumber();
-			matrix.get(fromNumber).add(toNode);
-			fromNode.setTriggerEvent(event, toNumber);
+			addEvent(event);
+			StateNode fromState = getStateNodeByState(from);
+			StateNode toState = getStateNodeByState(to);
+			matrix.get(fromState.getStateNumber()).set(getEventIndexFromEventMap(event), toState.getStateNumber());
 		}
 	}
 
 	/**
-	 * Create a transition given an event name from each state (except targetState) to targetState.
-	 * @param event the triggering event's name
-	 * @param targetState the target state to transit to
+	 * Point all states to a specific state(@param targetState), and call this event @param event.
+	 * If there are state that does not want to point to a specific state, use @param excepts to filter out these states.
+	 * If @param excepts is empty, it will not filter any state.
+	 *
+	 * e.g. the matrix look like:
+	 *   |1,2,3,4,5
+	 * 	===========
+	 *  1|1,1,1,1,5
+	 * 	2|2,2,2,2,5
+	 * 	3|3,3,3,3,5
+	 * 	4|4,4,4,4,5
+	 * 	5|5,5,5,5,5
+	 *
+	 * 	and call addTransitionFromAllStates("ALL", 3, 5), and matrix look like:
+	 *   |1,2,3,4,5
+	 * 	===========
+	 *  1|1,1,3,1,5
+	 * 	2|2,2,3,2,5
+	 * 	3|3,3,3,3,5
+	 * 	4|4,4,3,4,5
+	 * 	5|5,5,5,5,5
+	 * @param event event name
+	 * @param targetState target state
+	 * @param excepts those that want to be filtered out.
 	 */
-	public void addTransitionFromAllStates(String event, T targetState, T ...excepts) {
-		if(stateNodesMap.get(targetState.hashCode()) == null) {
-			throw new FiniteStateMachineException("targetState not exists");
-		}else {
-			StateNode targetNode = stateNodesMap.get(targetState.hashCode());
-			List<StateNode> exceptsNodeList = new ArrayList<>();
-			for (T except : excepts) {
-				StateNode exceptsNode = stateNodesMap.get(except.hashCode());
-				exceptsNodeList.add(exceptsNode);
-			}
-			for (int i = 0; i < matrix.size(); i++) {
-				if (i != targetNode.getStateNumber()) {
-					matrix.get(i).add(targetNode);
+	public void addTransitionFromAllStates(String event, T targetState, T ...excepts){
+		if(!isStateExists(targetState)){
+            throw new FiniteStateMachineException("targetState not exists");
+		}else{
+			addEvent(event);
+			StateNode targetStateNode = getStateNodeByState(targetState);
+			if(excepts.length > 0){
+				List<Integer> exceptsNodeList = new ArrayList<>();
+				for (T exceptState : excepts){
+					StateNode exceptsNode = getStateNodeByState(exceptState);
+					exceptsNodeList.add(exceptsNode.getStateNumber());
 				}
-			}
-			for (int i = 0; i < matrix.size(); i++) {
-				for (StateNode exceptsListNode : exceptsNodeList) {
-					if (i == exceptsListNode.getStateNumber()) {
-						matrix.get(i).remove(targetNode);
+				for (int i = 0; i < matrix.size(); i++){
+					if(!exceptsNodeList.contains(i)){
+						matrix.get(i).set(getEventIndexFromEventMap(event), targetStateNode.getStateNumber());
 					}
 				}
-			}
-			for (ArrayList<StateNode> stateMatrix : matrix) {
-				for (StateNode stateNodeMatrix : stateMatrix) {
-					stateNodeMatrix.setTriggerEvent(event, targetNode.getStateNumber());
+			}else {
+				for (ArrayList<Integer> state : matrix){
+					state.set(getEventIndexFromEventMap(event), targetStateNode.getStateNumber());
 				}
 			}
 		}
@@ -186,41 +316,16 @@ public class FiniteStateMachine<T> implements Cloneable{
 		}
 	}
 
-	class StateNode {
-
+	private class StateNode {
 		private int stateNumber;
-
 		private T state;
 
-		/**
-		 * triggerMap saves this corresponding node of event by this node
-		 */
-		private Map<String, Integer> triggerMap = new HashMap<String, Integer>();
-
-		private StateNode(T state, int stateNumber) {
+		private StateNode(T state, int stateNumber){
 			this.state = state;
 			this.stateNumber = stateNumber;
 		}
 
-		/**
-		 * Set trigger event in this node
-		 * @param event the triggering event's name
-		 * @param targetNumber the target number
-		 */
-		public void setTriggerEvent(String event, int targetNumber){
-			triggerMap.put(event, targetNumber);
-		}
-
-		/**
-		 * Through event to obtain the target number by triggerMap in this node
-		 * @param event the triggering event's name
-		 * @return state number
-		 */
-		public Integer getTargetNumberByCurrentNode(String event){
-			return triggerMap.get(event);
-		}
-
-		public int getStateNumber() {
+		private int getStateNumber() {
 			return stateNumber;
 		}
 
@@ -228,6 +333,4 @@ public class FiniteStateMachine<T> implements Cloneable{
 			return state;
 		}
 	}
-
-
 }
