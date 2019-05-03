acca4f8f1a4b83d8566c39db7849b5508134d888
johnny850807 <johnny850807@gmail.com>
2019-05-04 03:30:04 +0800

    FileUtility.read(path)

--- bow/bow-engine/src/main/java/com/pokewords/framework/engine/utils/FileUtility.java
+++ bow/bow-engine/src/main/java/com/pokewords/framework/engine/utils/FileUtility.java
@@ -1,8 +1,12 @@
 package com.pokewords.framework.engine.utils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class FileUtility {
-    public static String read(String path){
-        //TODO
-        return "";
+    public static String read(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        return new String(bytes);
     }
 }
