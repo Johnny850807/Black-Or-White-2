003aeaf863a76983d4578e804cb0b365007180cb
shawnwu4mac <shawnishere0806>
2019-05-13 22:59:46 +0800

    shutgun

---
+++ bow/bow-engine-demo/src/main/java/basics/states/App.java
@@ -0,0 +1,31 @@
+package basics.states;
+
+
+//import com.pokewords.framework.views.GameApplication;
+//
+//import java.awt.*;
+//
+//public class App extends GameApplication {
+//
+//    public static void main(String[] args) {
+//
+//        App app = new App();
+//        app.setWindowSize(1000,1000);
+//        app.setWindowbackgroundColor(Color.BLACK);
+//        app.setWindowName("8888");
+//        app.launch();
+//
+//    }
+//
+//    public void onAppInit() {
+//
+//    }
+//
+//    public void onAppLoading() {
+//
+//    }
+//
+//    public void onAppStarted() {
+//
+//    }
+//}
