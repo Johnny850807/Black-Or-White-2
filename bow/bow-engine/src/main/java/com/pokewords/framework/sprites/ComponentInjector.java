4a090e0748795a1eedaefdc571dee168f1043efe
johnny850807 <johnny850807@gmail.com>
2019-05-14 21:38:20 +0800

    componentInjector comment revision

--- bow/bow-engine/src/main/java/com/pokewords/framework/sprites/ComponentInjector.java
+++ bow/bow-engine/src/main/java/com/pokewords/framework/sprites/ComponentInjector.java
@@ -8,9 +8,8 @@ public class ComponentInjector {
 
 	/**
 	 * Inject the Sprite's components via reflection,
-	 * this method will locate each field of each component belongs to the sprite,
-	 * and inject the field if it's of Sprite type or if it's of the Component type that
-	 * the sprite does have.
+	 * this method will locate each field of each component,
+	 * if a field is of Sprite type, then inject that sprite instance in.
 	 * @param sprite the injected Sprite
 	 */
 	public static void inject(Sprite sprite) {
