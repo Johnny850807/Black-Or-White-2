6a24000463390fbbe681231df18012b95604eae9
nyngwang <nyngwang@gmail.com>
2019-05-18 21:04:18 +0800

    pull system-design

---
+++ bow/bow-engine/src/test/java/com/pokewords/framework/engine/GameEngineWeaverNodeTest.java
@@ -0,0 +1,17 @@
+package com.pokewords.framework.engine;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author nyngwang
+ */
+public class GameEngineWeaverNodeTest {
+
+    @Test
+    public void onWeaving() {
+
+        // effectPropertiesElement那邊的orElse是沒有的話使用原值？
+    }
+}
