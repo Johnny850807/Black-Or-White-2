04d2f42d7732c10b8e030517deeef0dfbe15a3f0
johnny850807 <johnny850807@gmail.com>
2019-05-21 21:58:28 +0800

    idk

--- bow/bow-engine-demo/src/main/java/homework/PlayerMouseListener.java
+++ bow/bow-engine-demo/src/main/java/homework/PlayerMouseListener.java
@@ -8,10 +8,40 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 
 @SuppressWarnings("Duplicates")
-public class PlayerMouseListener implements MouseListenerComponent.Listener {
+public class PlayerMouseListener extends MouseListenerComponent.Listener {
+    @Override
+    public void onMousePressed(Sprite sprite, Point mousePositionInWorld, Point mousePositionInArea) {
+        super.onMousePressed(sprite, mousePositionInWorld, mousePositionInArea);
+        System.out.println("press");
+    }
+
+    @Override
+    public void onMouseReleased(Sprite sprite, Point mousePositionInWorld, Point mousePositionInArea) {
+        super.onMouseReleased(sprite, mousePositionInWorld, mousePositionInArea);
+        System.out.println("released");
+    }
+
+    @Override
+    public void onMouseClicked(Sprite sprite, Point mousePositionInWorld, Point mousePositionInArea) {
+        super.onMouseClicked(sprite, mousePositionInWorld, mousePositionInArea);
+        System.out.println("clicked");
+    }
 
     @Override
     public void onMouseDragged(Sprite sprite, Point mousePositionInWorld, Point mousePositionInArea) {
-        sprite.setPosition(mousePositionInWorld);
+        super.onMouseDragged(sprite, mousePositionInWorld, mousePositionInArea);
+        System.out.println("dragged");
+    }
+
+    @Override
+    public void onMouseEnter(Sprite sprite, Point mousePositionInWorld, Point mousePositionInArea) {
+        super.onMouseEnter(sprite, mousePositionInWorld, mousePositionInArea);
+        System.out.println("enter");
+    }
+
+    @Override
+    public void onMouseExit(Sprite sprite, Point mousePositionInWorld) {
+        super.onMouseExit(sprite, mousePositionInWorld);
+        System.out.println("exit");
     }
 }
