2b233249959ab980c6b3c617121154fd047c9b57
nyngwang <nyngwang@gmail.com>
2019-05-25 04:56:39 +0800

    Complete Script.

--- bow/bow-engine/src/main/java/com/pokewords/framework/sprites/parsing/Script.java
+++ bow/bow-engine/src/main/java/com/pokewords/framework/sprites/parsing/Script.java
@@ -9,57 +9,29 @@ import java.util.stream.Collectors;
 public abstract class Script implements Node {
     protected List<Segment> segments;
 
-    public Script() {
-
+    public Script(List<Segment> segments) {
+        this.segments = segments;
     }
 
-    public Segment addSegment(Segment segment) {
+    public void addSegment(Segment segment) {
         segments.add(segment);
         segment.setParent(this);
-        return this;
     }
 
-    protected List<Segment> getSegments() {
+    public List<Segment> getSegments() {
         return segments;
     }
 
-    protected boolean containsSegment(String name) {
+    public boolean containsSegment(String name) {
         for (Segment segment : segments)
             if (segment.getName().equals(name))
                 return true;
         return false;
     }
 
-    protected boolean containsSegmentId(int id) {
-        for (Segment segment : segments)
-            if (segment.getId() == id)
-                return true;
-        return false;
-    }
-
-    protected boolean containsSegmentDescription(String description) {
-        for (Segment segment : segments)
-            if (segment.getDescription().equals(description))
-                return true;
-        return false;
-    }
-
-    protected List<Segment> getSegments(String name) {
+    public List<Segment> getSegments(String name) {
         return segments.stream()
                 .filter(segment -> segment.getName().equals(name))
                 .collect(Collectors.toList());
     }
-
-    protected List<Segment> getSegmentsById(int id) {
-        return segments.stream()
-                .filter(segment -> segment.getId() == id)
-                .collect(Collectors.toList());
-    }
-
-    protected List<Segment> getSegmentsByDescription(String description) {
-        return segments.stream()
-                .filter(segment -> segment.getDescription().equals(description))
-                .collect(Collectors.toList());
-    }
-
 }
