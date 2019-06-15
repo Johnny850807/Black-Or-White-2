cc6b02243ddcd483b82280a641491f8f28603cdb
johnny850807 <johnny850807@gmail.com>
2019-06-15 20:54:40 +0800

    add AiComponent

---
+++ bow/bow-engine/src/main/java/com/pokewords/framework/sprites/components/ai/AiComponent.java
@@ -0,0 +1,12 @@
+package com.pokewords.framework.sprites.components.ai;
+
+import com.pokewords.framework.sprites.components.CloneableComponent;
+
+public class AiComponent extends CloneableComponent {
+    private BehaviorTree behaviorTree;
+
+    @Override
+    public void onUpdate(double timePerFrame) {
+        // TODO
+    }
+}
