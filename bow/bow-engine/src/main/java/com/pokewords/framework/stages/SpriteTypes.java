b862fe7dde7eb1126a4b31b532544d382fe9e9db
shawnwu4mac <shawnishere0806>
2019-07-03 21:37:17 +0800

    tank script done

---
+++ bow/bow-engine/src/main/java/com/pokewords/framework/stages/SpriteTypes.java
@@ -0,0 +1,24 @@
+package com.pokewords.constants;
+
+public enum  SpriteTypes {
+    ROOT,  // the root of all types
+
+    MENU,
+    MENU_PLAY_BUTTON,
+    MENU_MULTIPLAYER_BUTTON,
+
+    CHARACTER,
+    HERO,
+    MONSTER,
+
+    PICKABLE,
+    PICKABLE_ITEM,
+    PICKABLE_GUN,
+    PICKABLE_PISTOL,
+    PICKABLE_MACHINE_GUN,
+    PICKABLE_SNIPER_RIFLE,
+
+    BULLET,
+    BULLET_BLUE_PELLET,
+    BULLET_FIRE_RIFLE;
+}
